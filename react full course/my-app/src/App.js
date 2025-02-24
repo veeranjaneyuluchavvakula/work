0be90ab71af254595b01c719adf616d29ae85fc9
veeranjaneyuluchavvakula <veeranjaneyuluchavvakula1@gmail.com>
2025-02-24 13:43:11 +0530

    newcommit

---
+++ react full course/my-app/src/App.js
@@ -0,0 +1,12 @@
+import React,{useState} from "react";
+
+function App() {
+  useState();
+  return (
+    <div className="App">
+      <h1>hello hooks</h1>
+    </div>
+  );
+}
+
+export default App;
