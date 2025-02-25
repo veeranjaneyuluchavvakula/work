125f2f4b3e4cb86142ce9babb96b63b41f2a118c
veeranjaneyuluchavvakula <veeranjaneyuluchavvakula1@gmail.com>
2025-02-25 18:58:07 +0530

    18:58

--- react full course/my-app/src/useState/final/2-useState-advanced.js
+++ react full course/my-app/src/useState/final/2-useState-advanced.js
@@ -1,33 +1,54 @@
 import React,{useState} from "react";
  
 const Index = () => {
-    const vse={
+    const initialArray=[{
+        id:456,
         firstName:"veera",
-        secondName:"karthik",
-        age:22,
-    }
-    const[data,setData]=useState(vse);
-    const changeFirstName=()=>{
-        setData({
-            ...data,
-            firstName:"sage it",    
-        })
-    }
-    const changesecondName =()=>{
-        setData({
-            ...data,
-            secondName:"ramu",
-        })
+     lastName:"anjanailu",
+        age:22
+    },
+    { 
+        id:123,
+      firstName:"karthik",
+     lastName:"akhilesh",
+      age:25
+    },
+    {
+        id:789,
+        firstName:"ram",
+        lastName:"aayodhaiya",
+        age:1990000,
     }
 
+    ];
+    const[data,setData]=useState(initialArray);
+    console.log(data);
+    const handleDelete =(comingId)=>{
+        const filterData=data.filter((eachItem)=>{
+        return eachItem.id !==comingId;
+    });
+    setData(filterData);
+    };
     return (
         <div>
-            <h1>My nameis {data.firstName}</h1>
-            <button onClick={changeFirstName}>change first name</button>
-            <h1>My name is {data.secondName}</h1>
-            <button onClick={changesecondName}>change lastname</button>
-            <h1>age:{data.age}</h1>
-
+           <ul>
+            {
+                data.map((eachItem,index)=>{
+                    const {firstName,lastName,age,id}=eachItem;
+                    return( <li key={index}>
+                        <div>
+                            firstName:<strong>{firstName}</strong>
+                        </div>
+                        <div>
+                            lastName:<strong>{lastName}</strong>
+                            </div>
+                        <div><strong>age:{age}</strong></div>
+                        <button onClick={()=>handleDelete(id)}>delete</button>
+                    </li>
+                );
+            })
+            }
+           </ul>
         </div>
     );
 }
