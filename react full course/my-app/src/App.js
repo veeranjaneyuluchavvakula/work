b681329b0ccbcb9cffaf9c651e7dc8f835ca6b7d
veeranjaneyuluchavvakula <veeranjaneyuluchavvakula1@gmail.com>
2025-03-04 19:08:03 +0530

    useEffect-example

--- react full course/my-app/src/App.js
+++ react full course/my-app/src/App.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
 import Sap from "./useState/final/1-useEffect-datafetch.js"
 
 
 function App() {
+  
 
   return (
     <div className="App">
--- react full course/my-app/src/useState/final/1-useEffect-datafetch.js
+++ react full course/my-app/src/useState/final/1-useEffect-datafetch.js
@@ -1,11 +1,60 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+
+const URL ="https://jsonplaceholder.typicode.com/users";
 
 const Assa =()=>{
-    return(
+    const [userData,setUsersData]=useState([]);
+    const[Loading,setLoading]=useState(false);
+    const[isError,setisError]=useState({status:false,msg:''})
+    const fetchUserData = async(apiURL)  =>  {
+       setLoading(true) 
+       setisError({status:false,msg:''})    
+        try{
+        const response = await fetch(apiURL);
+        const data = await response.json();
+
+        setUsersData(data);
+        setLoading(false)
+         setisError({status:false,msg:''});
+       if(response.status===404){
+        throw new Error("data not found");
+       }
+       } 
+     catch(error){
+        console.log(error.message);
+       setLoading(false)
+       setisError({status:true,msg: error.message||"something went,pls try again!"})
+     }
+        };
+      useEffect(()=>{
+      fetchUserData(URL)},[]);
+       if(Loading){
+     return(<div>
+        <h3>Loading...</h3>
+     </div>
+     );
+        }
+        if(isError?.status){
+            return<div>
+                <h3 style={{color:'red'}}>{isError?.msg}</h3>
+            </div>
+        }
+    
+     return(
         <div>
-            <h1>
-                veeras
-            </h1>
+            <h1>veeras</h1>
+            <ul>
+              {
+                userData.map((eachUser)=>{
+                    const {id,email,name}=eachUser;
+                   return<li key={id}>
+                    
+                    <div>{email}</div>
+                    <div>{name}</div>
+                    </li>
+                })
+              }  
+            </ul>
         </div>
     );
 
