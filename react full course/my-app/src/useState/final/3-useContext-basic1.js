b975f39c0b7f7dcd27fcc32de75ef99d3dcebe77
veeranjaneyuluchavvakula <veeranjaneyuluchavvakula1@gmail.com>
2025-03-20 19:45:40 +0530

    usecontext

--- react full course/my-app/src/useState/final/3-useContext-basic1.js
+++ react full course/my-app/src/useState/final/3-useContext-basic1.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const ParentComponent=()=>{
+  const[userDetails,setuserDetails]= useState({
+    firstName:"emma",
+    lastName:"watson",
+    email:"emma@gmail.com",
+   });
+
     return(
   <div>
-    <h1>useContext</h1>
+    <h1>useContext Parent COmponent</h1>
+    <ChildComponent userDetails={userDetails}/>
   </div>
     );
 };
+const ChildComponent=(props)=>{
+  console.log(props.userDetails);
+  return(
+<div><h1>
+  ChildComponent</h1>
+  <SubChildComponent userDetails={props.userDetails}/>
+</div>
+  );
+};
+const SubChildComponent=({userDetails})=>{
+ 
+
+  return(
+    <div>
+      <h3>SubChildComponent</h3>
+      <div>name:{userDetails.firstName}</div>
+      <div>name:{userDetails.lastName}</div>
+      <div>name:{userDetails.email}</div>
+    </div>
+  );
+}
 export default ParentComponent;
